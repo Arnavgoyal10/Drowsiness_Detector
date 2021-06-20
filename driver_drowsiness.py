95bde3a09b281e6ef52925c6669fb1dc7889f31e
Dev Sajnani <DevSajnani@Devs-iMac.local>
2021-06-20 18:02:29 +0530

    update

--- driver_drowsiness.py
+++ driver_drowsiness.py
@@ -13,7 +13,7 @@ cap = cv2.VideoCapture(0)
 
 #Initializing the face detector and landmark detector
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("")
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 #status marking for current state
 
